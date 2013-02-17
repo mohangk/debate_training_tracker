16a5e9daf26a5e7aaa3468ae21105f60c139883f
Mohan Krishnan <mohangk@gmail.com>
2013-02-18 07:55:26 +0800

    Fix mailer default URL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,5 @@ AdiTracker::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'adi2013.herokuapp.com' }
 end
